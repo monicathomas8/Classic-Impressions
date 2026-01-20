a6c262c07ec869a7943c610bb057f4047d0bbe28
Monica Thomas <monicathomas8@icloud.com>
2026-01-20 16:43:37 +0000

    contat.html updated with place holder info, forms.py in contact app updated

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,11 @@ class ContactMessageForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ["name", "email", "subject", "message"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "subject": forms.TextInput(attrs={"class": "form-control"}),
+            "message": forms.Textarea(
+                attrs={"class": "form-control", "rows": 6}
+            ),
+        }
